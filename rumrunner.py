f6dede2edb92600a3039b1ef1e0635f704dea047
Sandip Bhattacharya <sandipb@sandipb.net>
2018-08-22 02:45:14 -0700

    Change the architecture of rumrunner to suit multi-threaded apps
    Use an inproc zmq socket to aggregate counters and gauges, and flush them to speakeasy at regular intervals

--- rumrunner.py
+++ rumrunner.py
@@ -1,6 +1,10 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
 import logging
 import time
-
+import threading
+import collections
 
 try:
     import ujson as json
@@ -13,38 +17,79 @@ import zmq
 
 logger = logging.getLogger(__name__)
 
+# Socket for communication between rumrunner instances and its agent
+AGENT_SOCKET = "inproc://rumrunner_agent"
+
+# How long does the agent block on the receive socket before attempting to flush
+AGENT_POLL_MILLIS = 500
+
+# how long to consolidate values before sending to speakeasy
+AGENT_FLUSH_INTERVAL_SECS = 5
+
+# If the sending socket has these many events in queue, it will keep dropping further events
+DEFAULT_HWM = 10000
+
+METRIC_SUM, METRIC_COUNT = 0, 1
+
 
 class Rumrunner(object):
     MOCK = False
 
     def __init__(self, metric_socket, app_name, hwm=5000, block=False,
-                 strict_check_socket=True):
+                 strict_check_socket=True, start_agent=True,
+                 agent_socket=AGENT_SOCKET,
+                 flush_interval=AGENT_FLUSH_INTERVAL_SECS):
         self.metric_socket = metric_socket
+        self.hwm = hwm
+        self.flush_interval = flush_interval
         self.app_name = app_name
         self.block = block
+        self.agent_quit = threading.Event()
+        self.context = zmq.Context.instance()
+        self.agent_socket = agent_socket
 
-        self.context = zmq.Context()
+        self.setup_agent()
 
         # Send metrics
         self.send_socket = self.context.socket(zmq.PUSH)
         self.send_socket.set_hwm(hwm)
-        self.send_socket.connect('ipc://{0}'.format(self.metric_socket))
+        self.send_socket.connect(self.agent_socket)
         self.send_socket.setsockopt(zmq.LINGER, 0)
-        if strict_check_socket:
-            self.test_socket_writable(strict_check_socket)
+
+        # start the agent
+        if self.agent and start_agent:
+            self.start_agent()
+
+    def setup_agent(self):
+        "Stores an agent instance in self.agent if all is good, else None"
+        try:
+            self.agent_quit.clear()
+            self.agent = RumrunnerAgent(self.metric_socket, self.agent_quit,
+                                        self.hwm,
+                                        flush_interval=self.flush_interval,
+                                        agent_socket=self.agent_socket,
+                                        block=self.block)
+        except RumRunnerAgentError as e:
+            logger.debug("Ignoring agent setup error: %s", e)
+            self.agent = None
+
+    def start_agent(self):
+        "We only recreate agent if a previous instance was successfully created"
+        if self.agent and self.agent.is_dead():
+            logging.info("Attempting to recreate dead rumrunner agent")
+            self.setup_agent()
+        if self.agent is not None:
+            threading.Thread(name="RumrunnerAgent", target=self.agent.run).start()
+
+    def shutdown(self):
+        "Shutdown the agent"
+        self.agent_quit.set()
 
     def __new__(cls, *args, **kwargs):
         if cls.MOCK:
             return MockRumrunner(*args, **kwargs)
         else:
             return super(Rumrunner, cls).__new__(cls)
-
-    def test_socket_writable(self, strict):
-        tracker = self.send_socket.send('', copy=False, track=True)
-        try:
-            tracker.wait(3)
-        except zmq.NotDone:
-            raise Exception('Metric socket not writable')
 
     def counter(self, metric_name, value=1):
         return self.send(metric_name, value, 'COUNTER')
@@ -67,6 +112,193 @@ class Rumrunner(object):
             # Failed to send message
             logger.debug("Metric socket error - {0}".format(e))
             return False
+
+
+class RumRunnerAgentError(Exception):
+    pass
+
+
+class RumrunnerAgent(object):
+    """Receive data from all threads in the current process and send consolidated data to speakeasy
+
+    There should be only one thread of this class running, as it uses data structures without locks.
+    This is gated by the ownership of a zmq inproc socket in the global zmq context.
+    """
+    def __init__(self, metric_socket, quit_event,
+                 hwm=DEFAULT_HWM, strict_check_socket=True,
+                 flush_interval=AGENT_FLUSH_INTERVAL_SECS,
+                 agent_socket=AGENT_SOCKET,
+                 block=False):
+        self.metric_socket = metric_socket
+        self.quit_event = quit_event
+        self.flush_interval = flush_interval
+        self.last_run = time.time()
+        self.agent_socket = agent_socket
+        self.block = block
+
+        context = zmq.Context.instance()
+        self.metrics = {}
+
+        # socket to receiving metrics from local rumrunner
+        self.recv_socket = context.socket(zmq.PULL)
+        try:
+            self.recv_socket.bind(self.agent_socket)
+        except zmq.ZMQError:
+            raise RumRunnerAgentError(
+                "Error binding to agent socket at " + self.agent_socket + ". Is actually normal if you have created "
+                "more than one rumrunner instance in the same process")
+
+        # socket to send metrics to speakeasy
+        self.send_socket = context.socket(zmq.PUSH)
+        self.send_socket.set_hwm(hwm)
+        self.send_socket.connect('ipc://{0}'.format(self.metric_socket))
+        self.send_socket.setsockopt(zmq.LINGER, 0)
+        if strict_check_socket:
+            self.test_socket_writable(strict_check_socket)
+
+    def test_socket_writable(self, strict):
+        "Send a confirmed write to the remote socket to verify the address works"
+        tracker = self.send_socket.send('', copy=False, track=True)
+        try:
+            tracker.wait(3)
+        except zmq.NotDone:
+            raise RumRunnerAgentError('Metric socket not writable')
+
+    def run(self):
+        "Keep reading from socket till flush interval reached"
+        if not self.recv_socket:
+            raise Exception("No receive socket present (A shutdown agent cannot be restarted)")
+
+        logging.debug("Starting rumrunner agent loop")
+        while not self.quit_event.is_set():
+            self._run_once()
+        self.recv_socket.unbind(self.agent_socket)
+        self.recv_socket = None
+        logging.debug("Unbinding and exiting rumrunner agent loop")
+
+    def is_dead(self):
+        return self.recv_socket is None
+
+    def _run_once(self):
+        while time.time() < (self.last_run + self.flush_interval):
+            events_waiting = self.recv_socket.poll(AGENT_POLL_MILLIS)
+            if events_waiting:
+                try:
+                    self.process_data(self.recv_socket.recv())
+                except Exception as e:
+                    logging.error("Error aggregating data in RumrunnerAgent loop: %s", e)
+                    continue
+        try:
+            self.send_metrics()
+        except Exception as e:
+            logging.error("Error sending data in RumrunnerAgent loop: %s", e)
+
+    def init_app(self, app):
+        self.metrics[app] = {
+            'GAUGE': collections.defaultdict(lambda: [0, 0]),
+            'COUNTER': collections.defaultdict(lambda: [0, 0]),
+            'PERCENTILE': collections.defaultdict(list)
+        }
+
+    def process_data(self, data):
+        "Process a data frame received in the incoming socket"
+        try:
+            if not data:  # empty packets
+                logger.warn("Ignoring empty packet")
+                return
+            event = json.decode(data)
+            app_name, metric_name, metric_type, metric_value = event
+            if app_name not in self.metrics:
+                self.init_app(app_name)
+            # logging.info("Processing a=%s m=%s t=%s v=%f", app_name, metric_name, metric_type, metric_value)
+
+            # we store the sum of the value received for a metric for gauges and counters,
+            # as well as the number of such metrics received. For percentiles, we keep all
+            # the values in a list. The overall idea is that only gauges and counters are to
+            # be collapsed into a single data point, but the percentiles are passed on as-is.
+            if metric_type in ("GAUGE", "COUNTER"):
+                self.metrics[app_name][metric_type][metric_name][METRIC_SUM] += metric_value
+                self.metrics[app_name][metric_type][metric_name][METRIC_COUNT] += 1
+            elif metric_type == "PERCENTILE":
+                self.metrics[app_name][metric_type][metric_name].append(metric_value)
+            else:
+                logger.error("Ignoring invalid metric type '%s' for app=%s, metric=%s",
+                             metric_type, app_name, metric_name)
+        except (ValueError, IndexError):
+            logger.error("Ignoring malformed data which could not be decoded")
+            return
+
+    def collect_stats(self):
+        """
+        Collect stats about the number of metrics aggregate - at overall and app level::
+
+            rumrunner._all_.COUNTER.unique_total  = number of unique counters emitted
+            rumrunner._all_.COUNTER.aggregate_total  = number of counter values collapsed into the above unique values
+            ...
+
+        For percentiles, while the metrics are made available the same way, it should be noted
+        that there is no collapse/aggregation, and so the metrics don't intend to show any savings,
+        just occurences.
+        """
+        self.init_app("rumrunner")
+        UNIQUE, AGGREGATE = 0, 1
+        # overall metrics
+        all_stats = {"COUNTER": [0, 0], "GAUGE": [0, 0], "PERCENTILE": [0, 0]}
+
+        for app in self.metrics:
+            # app level metrics
+            app_stats = {"COUNTER": [0, 0], "GAUGE": [0, 0], "PERCENTILE": [0, 0]}
+            if app == "rumrunner":  # the stats we show ignore our internal metrics
+                continue
+            for metric_type in self.metrics[app]:
+                for metric in self.metrics[app][metric_type]:
+                    aggregate_value = (len(self.metrics[app][metric_type][metric])
+                                       if metric_type == "PERCENTILE"
+                                       else self.metrics[app][metric_type][metric][METRIC_COUNT])
+                    all_stats[metric_type][UNIQUE] += 1
+                    all_stats[metric_type][AGGREGATE] += aggregate_value
+                    app_stats[metric_type][UNIQUE] += 1
+                    app_stats[metric_type][AGGREGATE] += aggregate_value
+                self.metrics["rumrunner"]["COUNTER"]["rumrunner.{0}.{1}.unique_total".format(app, metric_type)] = [app_stats[metric_type][UNIQUE], 1]
+                self.metrics["rumrunner"]["COUNTER"]["rumrunner.{0}.{1}.aggregate_total".format(app, metric_type)] = [app_stats[metric_type][AGGREGATE], 1]
+        for metric_type in ("COUNTER", "GAUGE", "PERCENTILE"):
+            self.metrics["rumrunner"]["COUNTER"]["rumrunner._all_.{0}.unique_total".format(metric_type)] = [all_stats[metric_type][UNIQUE], 1]
+            self.metrics["rumrunner"]["COUNTER"]["rumrunner._all_.{0}.aggregate_total".format(metric_type)] = [all_stats[metric_type][AGGREGATE], 1]
+
+    def collapse_stats(self):
+        self.out_metrics = {}
+        for app in self.metrics:
+            for metric_type in self.metrics[app]:
+                for metric in self.metrics[app][metric_type]:
+                    val = self.metrics[app][metric_type][metric]
+                    if metric_type == "COUNTER":
+                        data_points = [val[METRIC_SUM]]
+                    elif metric_type == "GAUGE":
+                        data_points = [float(val[METRIC_SUM]) / val[METRIC_COUNT]]
+                    elif metric_type == "PERCENTILE":
+                        data_points = val
+                    else:
+                        continue  # ignore possible invalid type
+                    for dp in data_points:
+                        yield [app, metric, metric_type, dp]
+
+    def send_metrics(self):
+        """Gather some internal stats on aggregated metrics, and send them one by one for delivery.
+
+        Reset state after delivery.
+        """
+        self.last_run = time.time()
+        self.collect_stats()
+        for metric in self.collapse_stats():
+            self.send_to_speakeasy(metric)
+        self.metrics = {}
+
+    def send_to_speakeasy(self, data):
+        "Send a single metric to speakeasy. Don't block as speakeasy server may not be up"
+        if self.block:
+            self.send_socket.send(json.dumps(data))
+        else:
+            self.send_socket.send(json.dumps(data), zmq.NOBLOCK)
 
 
 class MockRumrunner(object):
@@ -95,14 +327,23 @@ def unmock_rumrunner():
 
 
 if __name__ == '__main__':
-    m = Rumrunner('/var/tmp/metric_socket', 'test.app')
+    stderr_handler = logging.StreamHandler()
+    stderr_handler.setFormatter(
+        logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S'))
+    logging.getLogger().addHandler(stderr_handler)
+    logging.getLogger().setLevel(logging.INFO)
+
+    m = Rumrunner('/var/tmp/metric_socket', 'test-app', flush_interval=2)
     s = time.time()
-    for x in range(1000):
-        if x % 100 == 0:
-            print x
+    for x in range(100):
+        if x % 10 == 0:
+            print(".", sep="", end="")
         m.counter('test_counter', 1)
         m.gauge('test_gauge', x)
         m.percentile('test_percentile.', x)
         time.sleep(0.000001)
+    print()
     e = time.time()
-    print "Took {0:.3f}s".format(e-s)
+    print("Took {0:.3f}s".format(e - s))
+    time.sleep(5)
+    m.shutdown()
--- test.py
+++ test.py
@@ -4,39 +4,75 @@
 import unittest
 import random
 import os
+import logging
+import tempfile
+import shutil
+import time
+import collections
+
+try:
+    import ujson as json
+except ImportError:
+    try:
+        import simplejson as json
+    except ImportError:
+        import json
 
 import zmq
 
 from rumrunner import Rumrunner
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] (%(funcName)s) %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
 
 class TestRumrunner(unittest.TestCase):
+    def tearDown(self):
+        if hasattr(self, "cleanup"):
+            self.cleanup()
+
     def test_send_counter_metric(self):
+        def cleanup():
+            if rr:
+                rr.shutdown()
+            recv_socket.close()
+            if os.path.exists(tmp_metric_socket):
+                os.remove(tmp_metric_socket)
         ctx = zmq.Context()
         recv_socket = ctx.socket(zmq.PULL)
-        tmp_metric_socket = '/var/tmp/test_metric_{0}'.format(random.random())
+        tmp_metric_socket = '/var/tmp/test_metric_1_{0}'.format(random.random())
         recv_socket.bind('ipc://{0}'.format(tmp_metric_socket))
+        self.cleanup = cleanup
 
-        Rumrunner(tmp_metric_socket, 'test_app').counter('test_counter')
-        recv_socket.recv()  # suck out empty string for write test
-        self.assertEqual(recv_socket.recv(),
-                         '["test_app", "test_counter", "COUNTER", 1]')
-        os.remove(tmp_metric_socket)
+        rr = Rumrunner(tmp_metric_socket, 'test_app', start_agent=False, flush_interval=1, agent_socket="inproc://test_send_counter_metric")
+        empty = recv_socket.recv()  # suck out empty string for write test
+        self.assertEqual(empty, "", "Rumrunner ping should return empty message")
+        rr.counter('test_counter')
+        rr.agent._run_once()
+        self.assertGreater(recv_socket.poll(10), 0, "There should be at least one event received pretty quickly")
+        self.assertEqual(recv_socket.recv(), json.dumps(["test_app", "test_counter", "COUNTER", 1]))
 
     def test_error_out_on_not_writable_socket_disable(self):
+        def cleanup():
+            if os.path.exists(tmp_metric_socket):
+                os.chmod(tmp_metric_socket, 0644)
+                os.remove(tmp_metric_socket)
+
         ctx = zmq.Context()
         recv_socket = ctx.socket(zmq.PULL)
         tmp_metric_socket = '/var/tmp/test_metric_{0}'.format(random.random())
         recv_socket.bind('ipc://{0}'.format(tmp_metric_socket))
+        self.cleanup = cleanup
 
-        Rumrunner(tmp_metric_socket, 'test_app', strict_check_socket=False)
+        Rumrunner(tmp_metric_socket, 'test_app', strict_check_socket=False, start_agent=False)
         os.chmod(tmp_metric_socket, 0444)
 
         # Should not raise an exception due to permissions
-        Rumrunner(tmp_metric_socket, 'test_app', strict_check_socket=False)
-        os.remove(tmp_metric_socket)
+        Rumrunner(tmp_metric_socket, 'test_app', strict_check_socket=False, start_agent=False)
 
     def test_error_out_on_not_writable_socket(self):
+        self.skipTest("This test is not reliable - socket permissions are often not respected by OS")
         ctx = zmq.Context()
         recv_socket = ctx.socket(zmq.PULL)
         tmp_metric_socket = '/var/tmp/test_metric_{0}'.format(random.random())
@@ -49,18 +85,92 @@ class TestRumrunner(unittest.TestCase):
         os.remove(tmp_metric_socket)
 
     def test_mock_rumrunner(self):
+        def cleanup():
+            if rr:
+                rr.shutdown()
         from rumrunner import unmock_rumrunner, mock_rumrunner, MockRumrunner
-        self.assertTrue(isinstance(Rumrunner('/var/tmp/test', 'test_app',
-                                             strict_check_socket=False),
-                                   Rumrunner))
+        rr = Rumrunner('/var/tmp/test', 'test_app', strict_check_socket=False, start_agent=False)
+        self.assertTrue(isinstance(rr, Rumrunner))
+
         mock_rumrunner()
-        self.assertTrue(isinstance(Rumrunner('/var/tmp/test', 'test_app',
-                                             strict_check_socket=False),
-                                   MockRumrunner))
+        rr = Rumrunner('/var/tmp/test', 'test_app', strict_check_socket=False, start_agent=False)
+        self.assertTrue(isinstance(rr, MockRumrunner))
+
         unmock_rumrunner()
-        self.assertTrue(isinstance(Rumrunner('/var/tmp/test', 'test_app',
-                                             strict_check_socket=False),
-                                   Rumrunner))
+        rr = Rumrunner('/var/tmp/test', 'test_app', strict_check_socket=False, start_agent=False)
+        self.assertTrue(isinstance(rr, Rumrunner))
+
+    def test_rumrunner_agent(self):
+        tdir = tempfile.mkdtemp()
+        rr1 = None
+        rr2 = None
+
+        def cleanup():
+            shutil.rmtree(tdir)
+            if rr1:
+                rr1.shutdown()
+            if rr2:
+                rr2.shutdown()
+        self.cleanup = cleanup
+
+        tmp_metric_socket = os.path.join(tdir, 'test_metric_{0}'.format(random.random()))
+        # logging.info("Using socket file %s", tmp_metric_socket)
+        ctx = zmq.Context.instance()
+        recv_socket = ctx.socket(zmq.PULL)
+        recv_socket.bind('ipc://{0}'.format(tmp_metric_socket))
+        rr1 = Rumrunner(tmp_metric_socket, "appA", start_agent=False, flush_interval=2)  # this will bind but not start the agent thread
+        rr2 = Rumrunner(tmp_metric_socket, "appA")  # let us imagine another thread in the same process creates another instance
+        self.assertIsNotNone(rr1.agent, "The first rumrunner instance should get the socket")
+        self.assertIsNone(rr2.agent, "The second rumrunner instance should NOT get the socket")
+
+        for x in range(10):
+            # 10 counters from each thread should aggregate to 20
+            rr1.counter("aCounter")
+            rr2.counter("aCounter")
+            # 10 gauges from each thread should average to (10+15)*10/20=12.5
+            rr1.gauge("bGauge", 10)
+            rr2.gauge("bGauge", 15)
+            # percentiles should remain as such, [10, 15, ...], length being 20 elements
+            rr1.percentile("cPercentile", 10)
+            rr2.percentile("cPercentile", 15)
+
+        events = {}
+        event_count = [0]
+
+        def capture_event(data):
+            if data[0] != "rumrunner":
+                app, metric, mtype, val = data
+                if app not in events:
+                    events[app] = collections.defaultdict(list)
+                events[app][metric].append({"type": mtype, "value": val})
+                event_count[0] += 1  # workaround to get at the no-global variable just outside the scope
+
+        # override the  original method to capture the events sent
+        rr1.agent.send_to_speakeasy = capture_event
+
+        # run one collection loop of flush_interval seconds
+        rr1.agent.last_run = init_time = time.time() - 1
+        rr1.agent._run_once()
+        rr1.shutdown()
+        rr1.shutdown()
+        self.assertGreater(rr1.agent.last_run, init_time,
+                           "After a collection loop, the last run time should be updated")
+
+        # while True:
+        #     num_events = recv_socket.poll(500)
+        #     if num_events > 0:
+        #         data = recv_socket.recv()
+        #         if not len(data):
+        #             continue
+        #         events.append(json.loads(data))
+        #     else:
+        #         break
+        # print ">>>>>>>>>", events
+        self.assertEqual(event_count[0], 22,
+                         "Should return one data point each for gauge/counter and all the ones for percentile")
+        self.assertEqual(events["appA"]["aCounter"][0]["value"], 20)
+        self.assertEqual(events["appA"]["bGauge"][0]["value"], 12.5)
+        self.assertEqual(len(events["appA"]["cPercentile"]), 20)
 
 
 if __name__ == '__main__':
